99679d9db072e34eeb957cf64f94f44169b4e4f2
Lyric-Joseph <98564614+RasticRookie@users.noreply.github.com>
2025-11-16 18:27:41 -0600

    Update text and button click handlers in script.js

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ function showYogurtChoice() {
   reStart.style.display = "none";
   gameOver.style.display = "none";
 
-  document.getElementById("firstxt").innerHTML = "Today, Velenium wanted to get something sweet. She got two cups of frozen yogurt while you waited at the table. It confused you on how she got them, seeing as there was no...cashier...but you decided not to dwell on that fact. She came back with a blue cup that has sprinkles and chocolate yogurt and a red cup that has vanilla yogurt with blueberries.";
+  document.getElementById("firstxt").innerHTML = "Today, Velenium wanted to get something sweet. She got two cups of frozen yogurt while you waited at the table. It confused you on how she got them, seeing as there was no...cashier...but you decided not to dwell on that fact. She came back with a blue cup that has sprinkles and chocolate yogurt and a red cup that has vanilla yogurt with blueberries. Which do you want?";
 
   button1.style.display = "";
   button2.style.display = "";
@@ -220,8 +220,8 @@ function handleContinue() {
 continueButton.onclick = handleContinue;
 button1.onclick = redClick;
 button2.onclick = blueClick;
-button3.onclick = headbandClick;
-button4.onclick = cakeClick;
+button3.onclick = cakeClick;
+button4.onclick = headbandClick;
 if (button5) button5.onclick = kanyeClick;
 if (button6) button6.onclick = nanaClick;
 if (button7) button7.onclick = onepieceClick;
